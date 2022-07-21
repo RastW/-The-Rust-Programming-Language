b289bdca8efa769345f7d8daf6370b6fda429b1c
RastW <rast.leonhart@qq.com>
2022-07-21 23:19:03 +0800

    up

--- book_16/send_sync/src/main.rs
+++ book_16/send_sync/src/main.rs
@@ -2,5 +2,13 @@ use std::marker::Sync;  // 允许多线程同时访问，lock用
 use std::marker::Send;  // 允许线程间转移所有权，channel用
 
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello");
+    let s1 = exec(s);
+
+    print!("{}", s);
+    print!("{}", s1);
+}
+
+fn exec(s: String) -> String{
+    s
 }
