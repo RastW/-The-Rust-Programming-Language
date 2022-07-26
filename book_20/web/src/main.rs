1206751d6f9e74d40640166c771c197376b8b5a3
RastW <rast.leonhart@qq.com>
2022-07-26 21:17:41 +0800

    up

--- book_20/web/src/main.rs
+++ book_20/web/src/main.rs
@@ -1,38 +1,45 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, fs};
+use std::{
+    fs,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for  stream in listener.incoming() {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-    
+
         handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-    
+
     stream.read(&mut buffer).unwrap();
 
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = "GET / HTTP/1.1\r\n";
 
-    // 请求
-    // Method Request-URI HTTP-version CRLF
+    if buffer.starts_with(get.as_bytes()) {
+        println!("202!--");
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
 
-    // let contents = fs::read_to_string("hello.html").unwrap();
-    // let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        // 等待并阻止程序的运行，直到所有数据被写入
+        stream.flush().unwrap();
+    } else {
+        println!("404!--");
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{}{}", status_line, contents);
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-
-    // println!("{response}");
-
-    stream.write(response.as_bytes()).unwrap();
-    // 等待并阻止程序的运行，直到所有数据被写入
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+        // 等待并阻止程序的运行，直到所有数据被写入
+        stream.flush().unwrap();
+    }
 }
